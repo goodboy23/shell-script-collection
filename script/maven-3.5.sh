84f339ef6ebacac23410a4f92b6eebf92991c927
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-06-04 19:03:08 +0800

    Update maven-3.5.sh

--- script/maven-3.5.sh
+++ script/maven-3.5.sh
@@ -2,6 +2,15 @@
 
 
 
+#[使用设置]
+#主目录，相当于/usr/local
+#install_dir=
+
+#服务目录名
+maven_dir=maven3.5
+
+
+
 script_get() {
     test_package https://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/apache-maven-3.5.2-bin.tar.gz 948110de4aab290033c23bf4894f7d9a
 }
@@ -13,15 +22,17 @@ script_install() {
         exit
     fi
     
+    test_dir ${maven_dir}
+    
     script_get
     tar -xf package/apache-maven-3.5.2-bin.tar.gz
-    mv apache-maven-3.5.2 /usr/local/maven3.5
+    mv apache-maven-3.5.2 ${install_dir}/${maven_dir}
     
     #清理环境变量
     sed -i '/^export MAVEN_HOME=/d' /etc/profile
     sed -i '/^export PATH=${MAVEN_HOME}/d' /etc/profile
     
-    echo 'export MAVEN_HOME=/usr/local/maven3.5' >> /etc/profile
+    echo "export MAVEN_HOME=${install_dir}/${maven_dir}" >> /etc/profile
     echo 'export PATH=${MAVEN_HOME}/bin:${PATH}' >> /etc/profile
     source /etc/profile
     
@@ -29,13 +40,13 @@ script_install() {
     [[ $? -eq 0 ]] || print_error "maven-3.5安装失败，请检查脚本" "maven-3.5 installation failed, please check the script"
     
 	print_massage "maven-3.5安装完成" "The maven-3.5 is installed"
-	print_massage "安装目录：/usr/local/maven3.5" "Install Dir：/usr/local/maven3.5"
+	print_massage "安装目录：${install_dir}/${maven_dir}" "Install Dir：${install_dir}/${maven_dir}"
 	print_massage "使用：mvn -v" "Use：mvn -v"
 	print_massage "使用说明：maven环境部署" "Instructions for use: Maven environment deployment"
 }
 
 script_remove() {
-	rm -rf /usr/local/maven-3.5
+    rm -rf /usr/local/maven-3.5
     sed -i '/^export MAVEN_HOME=/d' /etc/profile
     sed -i '/^export PATH=${MAVEN_HOME}/d' /etc/profile
     
