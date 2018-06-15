34bec12dbe7afeaf7782ba122a366d1e320a77a6
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-06-15 17:25:32 +0800

    Update rvm.sh

--- script/rvm.sh
+++ script/rvm.sh
@@ -13,7 +13,8 @@ script_install() {
         exit
     fi
 
-    yum -y remove ruby
+    test_remove ruby
+    
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     \curl -sSL https://get.rvm.io | bash -s stable
     
